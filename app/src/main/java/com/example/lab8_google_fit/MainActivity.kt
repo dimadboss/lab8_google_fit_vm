9b270b4e9e9e85f31186e4abcba9943919d65954
D Drozdov <ds-drozdov@yandex.ru>
2022-12-11 20:57:13 +0300

    Initial commit

---
+++ app/src/main/java/com/example/lab8_google_fit/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.lab8_google_fit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.lab8_google_fit.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/com/example/lab8_google_fit/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.lab8_google_fit.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
