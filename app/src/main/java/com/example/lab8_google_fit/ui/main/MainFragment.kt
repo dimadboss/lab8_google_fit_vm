294256245ef27c96af5ef48d7b6dd18977626f66
D Drozdov <ds-drozdov@yandex.ru>
2022-12-12 12:28:28 +0300

    implement ui to add new steps record

--- app/src/main/java/com/example/lab8_google_fit/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/lab8_google_fit/ui/main/MainFragment.kt
@@ -1,23 +1,35 @@
 package com.example.lab8_google_fit.ui.main
 
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
+import android.icu.util.Calendar
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.text.format.DateFormat
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
-import android.widget.ListView
+import android.widget.*
 import androidx.lifecycle.Observer
 import com.example.lab8_google_fit.MainActivity
 import com.example.lab8_google_fit.R
 import com.example.lab8_google_fit.data.StepsData
+import java.time.LocalDateTime
 
-class MainFragment : Fragment() {
+class MainFragment : Fragment(), DatePickerDialog.OnDateSetListener,
+    TimePickerDialog.OnTimeSetListener {
 
     companion object {
         fun newInstance() = MainFragment()
     }
+
+    var myDay = 0
+    var myMonth: Int = 0
+    var myYear: Int = 0
+
+    var myDateTime: LocalDateTime? = null
+    var myStepsCount: Int? = null
 
     private lateinit var viewModel: MainViewModel
     private lateinit var mainActivity: MainActivity
@@ -43,6 +55,60 @@ class MainFragment : Fragment() {
         }
 
         viewModel.stepsListLive.observe(viewLifecycleOwner, nameObserver)
+
+        mainActivity.findViewById<TextView>(R.id.selectedDateTime).setOnClickListener {
+            val calendar: Calendar = Calendar.getInstance()
+            val datePickerDialog = DatePickerDialog(
+                mainActivity,
+                this,
+                calendar.get(Calendar.YEAR),
+                calendar.get(Calendar.MONTH),
+                calendar.get(Calendar.DAY_OF_MONTH),
+            )
+            datePickerDialog.show()
+        }
+
+        mainActivity.findViewById<SeekBar>(R.id.seekBar)
+            .setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+                override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+                    myStepsCount = progress
+                    updateUI()
+                }
+
+                override fun onStartTrackingTouch(seekBar: SeekBar) {
+                }
+
+                override fun onStopTrackingTouch(seekBar: SeekBar) {
+                }
+            })
+    }
+
+    override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+        myDay = dayOfMonth
+        myYear = year
+        myMonth = month + 1
+        val calendar = Calendar.getInstance()
+        val timePickerDialog = TimePickerDialog(
+            mainActivity,
+            this,
+            calendar.get(Calendar.HOUR),
+            0,
+            DateFormat.is24HourFormat(mainActivity),
+        )
+        timePickerDialog.show()
+    }
+
+    override fun onTimeSet(view: TimePicker?, hourOfDay: Int, minute: Int) {
+        myDateTime = LocalDateTime.of(myYear, myMonth, myDay, hourOfDay, 0, 0)
+        updateUI()
+    }
+
+    private fun updateUI() {
+        var title = myDateTime?.toString() ?: "Select date and time"
+        if (myStepsCount != null) {
+            title += " — $myStepsCount steps"
+        }
+        mainActivity.findViewById<TextView>(R.id.selectedDateTime).text = title
     }
 
     override fun onCreateView(
